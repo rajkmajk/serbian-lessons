ccc35915e7229e9b39a9aec646de7f2a4231dde2
Mihailo Rakic <mihailo.rakic@protonmail.com>
2023-10-09 16:30:29 -0400

    add .

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.getElementById('button').addEventListener('click', function() {
   
     let h2 = document.querySelector('h2');
-    let serbian = ["juče (yesterday)", "danas (today)", "sutra (tomorrow)", "dobro (good)", "loše (bad)", "dođi! (come here!)", "poljubiti (to kiss)", "posao (job)", "odlično! (great!)", "skupo (expensive)", "pijan (drank)", "jeftino (cheap)", "neverovatno (incredible)", "sto (table)", "stolica (chair)", "pantalone (pants)", "majica (t-shirt)", "netačno (false-not true)", "tačno (true)", "imati (to have)", "biti (to be)", "jabuka (apple)", "ručak (lunch)", "Da li je sve u redu?", "Kako si?", "Šta ima novo?", "osećati (to feel)", "jesti (to eat)", "plakati (to cry)", "pevati (to sing)", "spavati (to sleep)", "učiti srpski jezik (to learn serbian language)"];
+    let serbian = ["juče (yesterday)", "danas (today)", "sutra (tomorrow)", "dobro (good)", "loše (bad)", "dođi! (come here!)", "poljubiti (to kiss)", "posao (job)", "odlično! (great!)", "skupo (expensive)", "pijan (drunk)", "jeftino (cheap)", "neverovatno (incredible)", "sto (table)", "stolica (chair)", "pantalone (pants)", "majica (t-shirt)", "netačno (false-not true)", "tačno (true)", "imati (to have)", "biti (to be)", "jabuka (apple)", "ručak (lunch)", "Da li je sve u redu?", "Kako si?", "Šta ima novo?", "osećati (to feel)", "jesti (to eat)", "plakati (to cry)", "pevati (to sing)", "spavati (to sleep)", "učiti srpski jezik (to learn serbian language)"];
     let randomNum = Math.floor(Math.random() * (serbian.length));
     
     h2.innerText = serbian[randomNum];
